08eadb6481e68691bc09a0d4a8b210161ea84f54
hello <hello@DESKTOP-JM3TR6E>
2023-04-09 18:18:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Minecraft;
+
+/// <summary>
+/// The class containing the program entry point.
+/// </summary>
+public static class Program
+{
+    #region Public Static Methods
+
+    /// <summary>
+    /// The program entry point.
+    /// </summary>
+    public static void Main()
+    {
+        Console.WriteLine("Hello world!");
+    }
+
+    #endregion
+}
